24a44c62d964e3154e121387259fe07af8d5ddf4
Dikshanta <3108dikshanta@gmail.com>
2025-03-30 23:12:32 +0545

    User schemas updated

--- app/schemas/chat.py
+++ app/schemas/chat.py
@@ -5,3 +5,12 @@ class UserInput(BaseModel):
     message: Union[str, int]  # Allows both string and integer inputs
     role: str = "user"
     conversation_id: str
+
+
+'''
+{
+  "message": "Find the best books",
+  "role": "user",
+  "conversation_id": "12345"
+}
+'''
