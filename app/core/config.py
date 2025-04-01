93c5b6633517787c8dd61869fbb2df905127febf
Dikshanta <3108dikshanta@gmail.com>
2025-04-01 21:18:34 +0545

    config updated

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = os.getenv("GROQ_API_KEYs")
 
 if not GROQ_API_KEY:
     raise ValueError("API key for Groq is missing. Please set the GROQ_API_KEY in the .env file.")
